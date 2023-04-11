e2ea79613d81474495884cc3542835597b3235be
Alumno <Alumno@LAB1507-11>
2023-04-11 13:34:57 -0500

    First commit

---
+++ src/main/java/com/tecsup/lab03/lab03_4C24_GP05/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.lab03.lab03_4C24_GP05;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/tecsup/lab03/lab03_4C24_GP05/AppTest.java
@@ -0,0 +1,20 @@
+package com.tecsup.lab03.lab03_4C24_GP05;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
